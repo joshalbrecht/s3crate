133d0c6f4e8bc9ebc6e3c3fa77a8c62fdf7029b3
Josh Albrecht <thejash@gmail.com>
2013-09-18 15:48:24 +0000

    adding unfinished spec

---
+++ src/test/scala/com/codexica/s3crate/filetree/history/snapshotstore/s3/S3FileHistorySpec.scala
@@ -0,0 +1,27 @@
+package com.codexica.s3crate.filetree.history.snapshotstore.s3
+
+import com.codexica.common.SafeLogSpecification
+import org.scalamock.specs2.MockFactory
+
+/**
+ * @author Josh Albrecht (joshalbrecht@gmail.com)
+ */
+class S3FileHistorySpec extends SafeLogSpecification with MockFactory {
+
+  trait Context extends BaseContext {
+
+  }
+
+  "initializing the file history" should {
+    "make it possible to read the metadata" in new Context {
+
+    }
+  }
+
+  "calling update" should {
+    "save the new file snapshot" in new Context {
+
+    }
+  }
+
+}
