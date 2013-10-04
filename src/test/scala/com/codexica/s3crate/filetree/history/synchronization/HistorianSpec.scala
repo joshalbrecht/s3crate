7674a440e2da79e5efa888a275d9543dd4d4a4f7
Josh Albrecht <thejash@gmail.com>
2013-10-04 16:24:49 +0000

    finished testing historian class

--- src/test/scala/com/codexica/s3crate/filetree/history/synchronization/HistorianSpec.scala
+++ src/test/scala/com/codexica/s3crate/filetree/history/synchronization/HistorianSpec.scala
@@ -1,6 +1,6 @@
 package com.codexica.s3crate.filetree.history.synchronization
 
-import com.codexica.common.SafeLogSpecification
+import com.codexica.common.{InaccessibleDataError, SafeLogSpecification}
 import com.codexica.s3crate.filetree.history.{NoCompression, FilePathState, FileTreeHistory}
 import com.codexica.s3crate.filetree.{FileMetaData, PathGenerator, FileTreeListener, FilePathEvent, FilePath, FileTree}
 import org.joda.time.DateTime
@@ -23,45 +23,63 @@ class HistorianSpec extends SafeLogSpecification {
     val treeHistory = mock[FileTreeHistory]
     val path = FilePath("/some/path")
     val event = FilePathEvent(path, new DateTime())
-  }
+    def makeSnapshot(remoteFileExists: Boolean) = {
+      val state = FilePathState(path, remoteFileExists, None)
+      val blob = DataBlob("sfasd", None, NoCompression())
+      FileSnapshot(UUID.randomUUID(), blob, state, None)
+    }
 
-  "a missing file" should {
-    "cause a deletion in the history" in new Context {
-      fileTree.metadata(Matchers.eq(path)) returns Future {FilePathState(path, false, None)}
+    def singleFileCheck(localFileExists: Boolean, remoteFileExists: Boolean) {
+      fileTree.metadata(Matchers.eq(path)) returns Future {
+        FilePathState(path, localFileExists, None)
+      }
       treeHistory.metadata(Matchers.eq(path)) returns Future {
-        val state = FilePathState(path, true, None)
-        val blob = DataBlob("sfasd", None, NoCompression())
-        Option(FileSnapshot(UUID.randomUUID(), blob, state, None))
+        Option(makeSnapshot(remoteFileExists))
       }
       val historian = new Historian(fileTree, treeHistory, context)
       historian.onNewFilePathEvent(event)
       Await.result(historian.stop(), Duration.Inf)
+    }
+  }
+
+  "a missing file" should {
+    "cause a deletion in the history" in new Context {
+      singleFileCheck(false, true)
       there was one(treeHistory).delete(Matchers.eq(path))
     }
   }
 
   "a changed file" should {
     "cause a change in the history" in new Context {
-
+      singleFileCheck(true, true)
+      there was one(treeHistory).update(Matchers.eq(path), Matchers.eq(fileTree))
     }
   }
 
   "a new file" should {
     "cause a new file in the history" in new Context {
-
+      singleFileCheck(true, false)
+      there was one(treeHistory).update(Matchers.eq(path), Matchers.eq(fileTree))
     }
   }
 
   "throwing an invalid data exception while synching" should {
     "cause that event to be retried" in new Context {
-
+      var wasCalled = false
+      treeHistory.update(Matchers.eq(path), Matchers.eq(fileTree)).answers(
+        (p, t) => {
+          if (wasCalled) {
+            Future {
+              makeSnapshot(true)
+            }
+          } else {
+            wasCalled = true
+            throw new InaccessibleDataError("Whatever", null)
+          }
+        }
+      )
+      singleFileCheck(true, true)
+      there were two(treeHistory).update(Matchers.eq(path), Matchers.eq(fileTree))
     }
   }
-
-  "stopping the sync process" should {
-    "finish only after all tasks have completed" in new Context {
-
-    }
-  }
-
 }
